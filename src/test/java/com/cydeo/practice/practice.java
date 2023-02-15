f01f037799cde563603c9e2f61f9008ae8ca4206
NenadCvetic <cveticnenad92@gmail.com>
2023-02-15 20:33:11 +0100

    POST request practice

--- src/test/java/com/cydeo/practice/practice.java
+++ src/test/java/com/cydeo/practice/practice.java
@@ -8,13 +8,20 @@ import com.cydeo.utilities.ConfigurationReader;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.*;
+
+import static org.hamcrest.MatcherAssert.*;
+
+import static org.hamcrest.Matchers.*;
 
 public class practice extends BookITTestBase {
 
@@ -118,6 +125,82 @@ public class practice extends BookITTestBase {
         List<Location> list1 = jsonPath.getList("", Location.class);
 
         System.out.println(list1);
+
+
+    }
+
+
+    @DisplayName("POST request Conferences")
+    @Test
+    public void test5 () {
+
+        Map<String, Object> postRequestMap = new LinkedHashMap<>();
+
+        postRequestMap.put("conference-type", "solid");
+        postRequestMap.put("room-id",115);
+        postRequestMap.put("year", 2023);
+        postRequestMap.put("month", 5);
+        postRequestMap.put("day", 28);
+        postRequestMap.put("timeline-id",11243);
+        postRequestMap.put("notify-team", false);
+
+
+        JsonPath jsonPath = given().accept(ContentType.JSON)
+                .and()
+                .contentType(ContentType.JSON)
+                .and()
+                .headers("Authorization", BookITUtils.getTokenByRole("teacher"))
+                .queryParams(postRequestMap)
+                .log().all()
+                .and()
+                .when()
+                .post("/api/conferences/conference")
+                .then()
+                .statusCode(200)
+                .log().all().extract().jsonPath();
+
+        int conference_id = jsonPath.getInt("id");
+
+        int room_id = jsonPath.getInt("room.id");
+
+        String firstName = jsonPath.getString("reservator.firstName");
+
+        int timelineID = jsonPath.getInt("timelines[0].id");
+
+        int timelineHour = jsonPath.getInt("timelines[0].start.hour");
+
+
+        Response response = given().accept(ContentType.JSON)
+                .and()
+                .headers("Authorization", BookITUtils.getTokenByRole("teacher"))
+                .pathParam("id", conference_id)
+                .and()
+                .get("/api/conferences/{id}")
+                .then()
+                .statusCode(200)
+                .extract().response();
+
+        assertThat(response.path("type"),is(equalToIgnoringCase((String) postRequestMap.get("conference-type"))));
+
+        assertThat(response.path("room.id"),is(equalTo(postRequestMap.get("room-id"))));
+
+        assertThat(response.path("timelines[0].id"),is(equalTo(postRequestMap.get("timeline-id"))));
+
+
+        given().accept(ContentType.JSON)
+                .and()
+                .headers("Authorization",BookITUtils.getTokenByRole("teacher"))
+                .and()
+                .pathParam("id",conference_id)
+                .and()
+                .delete("/api/conferences/{id}")
+                .then()
+                .statusCode(204)
+                .log().all();
+
+
+
+
 
 
     }
